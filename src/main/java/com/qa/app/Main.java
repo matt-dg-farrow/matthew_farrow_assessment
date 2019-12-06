8daece4be0de9e9e6c57bc4bc50f32df9dcb47e6
Matthew Farrow <matt.dg.farrow@gmail.com>
2019-12-06 09:13:51 +0000

    First commit

---
+++ src/main/java/com/qa/app/Main.java
@@ -0,0 +1,9 @@
+package com.qa.app;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/test/java/com/qa/test/Test.java
@@ -0,0 +1,13 @@
+package com.qa.test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		assertEquals("THIS", "THIS");
+
+	}
+
+}
